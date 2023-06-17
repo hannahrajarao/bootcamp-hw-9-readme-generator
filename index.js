17b9bc58714cce5cab3bba7edda97fd8974d9a0f
hannahrajarao <hannahrajarao@gmail.com>
2023-06-16 23:08:11 -0400

    Inquirer prompt

--- index.js
+++ index.js
@@ -63,7 +63,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(response => {
+        console.log(response);
+    });
+}
 
 // Function call to initialize app
 init();

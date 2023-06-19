eb9ae8aefc31a2942b00ad74f6783dcfc2ced0fb
hannahrajarao <hannahrajarao@gmail.com>
2023-06-18 20:09:34 -0400

    Add markdown text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,6 +14,57 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+List your collaborators, if any, with links to their GitHub profiles.
+
+If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+
+If you followed tutorials, include links to those here as well.
+
+## License
+
+${renderLicenseSection(data.license)}
+
+## Badges
+
+${renderLicenseBadge(data.license)}
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+My GitHub profile is [https://github.com/${data.username}](https://github.com/${data.username})
+You can email me at [${data.email}](mailto:${data.email})
 `;
 }
 

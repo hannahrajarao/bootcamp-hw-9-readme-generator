e82cba0832ea730ae25add242dd1577e9e937397
hannahrajarao <hannahrajarao@gmail.com>
2023-06-18 20:34:53 -0400

    Remove sections from readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,6 @@ ${data.description}
 
 - [Installation](#installation)
 - [Usage](#usage)
-- [Credits](#credits)
 - [License](#license)
 
 ## Installation
@@ -33,14 +32,6 @@ ${data.installation}
 
 ${data.usage}
 
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
 ## License
 
 ${renderLicenseSection(data.license)}
@@ -48,10 +39,6 @@ ${renderLicenseSection(data.license)}
 ## Badges
 
 ${renderLicenseBadge(data.license)}
-
-## Features
-
-If your project has a lot of features, list them here.
 
 ## How to Contribute
 
